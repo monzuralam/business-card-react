8840de6cf540c4404207f43033534e972040989e
monzuralam <monzur707@gmail.com>
2023-06-05 08:15:22 +0600

    react-vite init with tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+class App extends Component {
+  render() {
+    return (
+      <>
+        <h1 className="text-3xl font-bold underline text-center my-4">
+          Hello world!
+        </h1>
+      </>
+    );
+  }
+}
+
+export default App;
